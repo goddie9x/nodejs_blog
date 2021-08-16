6a7cef2ac0c388a5ab7582024e6ce0319933d183
goddie9x <goddie9x@yahoo.com>
2021-08-16 21:34:32 +0700

    done firt nodejs course

--- src/app.js
+++ src/app.js
@@ -5,35 +5,62 @@ const handlebars = require('express-handlebars');
 const app = express();
 const port = 3000;
 const route = require('./routes');
+const db = require('./config/db');
+const methodOverride = require('method-override');
+const sortMiddleware = require('./app/middlewares/SortMiddleware');
+
+db.connect();
 
 app.use(express.static(path.join(__dirname, 'public')));
 /* app.use(morgan('combined')); */
 
 app.use(
- express.urlencoded({
-  extended: true,
- }),
+    express.urlencoded({
+        extended: true,
+    }),
 );
 
 app.use(express.json());
 
 app.engine(
- 'tam',
- handlebars({
-  extname: '.tam',
- }),
-);
-app.set('view engine', 'tam');
-app.set('views', path.join(__dirname, 'resource\\views'));
+    'tam',
+    handlebars({
+        extname: '.tam',
+        helpers: {
+            sum: (a, b) => a + b,
+            sortable: (field, sort) => {
+                const sortType = field == sort.column ? sort.type : 'default';
 
+                const classTypes = {
+                    default: '',
+                    asc: 'asc',
+                    desc: 'desc',
+                }
+                const types = {
+                    asc: 'desc',
+                    desc: 'asc',
+                    default: 'desc',
+                }
+
+                classType = classTypes[sortType];
+                nextType = types[sortType];
+
+                return `href="?_sort&column=${field}&type=${nextType}" class="${classType} ms-3"`;
+            }
+        },
+    }),
+);
+
+app.set('view engine', 'tam');
+app.set('views', path.join(__dirname, 'resource', 'views'));
+
+app.use(methodOverride('_method'));
+
+//use custom middlewares
+app.use(sortMiddleware);
 //Route
 route(app);
-/* 
-app.post('/search', (req, res) => {
-    console.log(req.body);
-    res.render('search');
-}); */
 
 app.listen(port, () => {
- console.log(`app listening at http://localhost:${port}`);
+    console.log(`app listening at http://localhost:${port}`);
 });
